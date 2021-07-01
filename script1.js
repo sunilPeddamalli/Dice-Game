ac5210183e0ce2cf9fd534c6d39aa5fd3ca6aa76
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-07-01 14:37:44 -0700

    On hold button click event- hold the score of the current player and switch to the next player if score is less than 20

--- script1.js
+++ script1.js
@@ -2,9 +2,20 @@ const rollDice = document.querySelector('.btn--roll');
 const dice = document.querySelector('.dice');
 const currentPlayer1 = document.querySelector('#current--0');
 const currentPlayer2 = document.querySelector('#current--1');
+const holdScore = document.querySelector('.btn--hold')
+const scorePlayer1 = document.querySelector('#score--0');
+const scorePlayer2 = document.querySelector('#score--1');
 
+let score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+};
 
 rollDice.addEventListener('click', () => {
     dice.classList.remove('hidden');
@@ -15,9 +26,16 @@ rollDice.addEventListener('click', () => {
         currentScore += randomNum;
         document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     } else {
-        document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+        switchPlayer();
+    }
+});
+
+holdScore.addEventListener('click', () => {
+    score[`${activePlayer}`] += currentScore
+    document.querySelector(`#score--${activePlayer}`).textContent = score[`${activePlayer}`];
+    if (score[`${activePlayer}`] < 20) {
+        switchPlayer();
+    } else {
+        console.log('Score >= 20');
     }
 });
