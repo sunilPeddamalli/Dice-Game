fbebe4e9e007b5c0c3f008b90e15c5bfa88bd9d6
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-07-08 16:55:04 -0700

    implemented NewButton functionality to reset the game. Also added setTimeout function to hide dice to switchplayer functionality

--- script1.js
+++ script1.js
@@ -7,6 +7,7 @@ const scorePlayer1 = document.querySelector('#score--0');
 const scorePlayer2 = document.querySelector('#score--1');
 const player1 = document.querySelector('.player--0 ');
 const player2 = document.querySelector('.player--1')
+const newGame = document.querySelector('.btn--new');
 
 let score = [0, 0];
 let currentScore = 0;
@@ -20,13 +21,17 @@ const switchPlayer = function () {
     document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     player1.classList.toggle('player--active');
     player2.classList.toggle('player--active');
+    setTimeout(function () {
+        dice.classList.add('hidden');
+        gameState = true;
+    }, 500);
+    gameState = false;
 };
 
 rollDice.addEventListener('click', () => {
     if (gameState) {
         dice.classList.remove('hidden');
         const randomNum = Math.trunc(Math.random() * 6) + 1;
-        console.log(randomNum);
         dice.src = `dice-${randomNum}.png`
         if (randomNum !== 1) {
             currentScore += randomNum;
@@ -46,8 +51,22 @@ holdScore.addEventListener('click', () => {
         } else {
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-            dice.classList.add('hidden');
             gameState = false;
         }
     }
+});
+
+newGame.addEventListener('click', () => {
+    gameState = true;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+    player1.classList.add('player--active');
+    player2.classList.remove('player--active');
+    dice.classList.add('hidden');
+    currentScore = 0;
+    activePlayer = 0;
+    score = [0, 0];
+    currentPlayer1.textContent = 0;
+    currentPlayer2.textContent = 0;
+    scorePlayer1.textContent = 0;
+    scorePlayer2.textContent = 0;
 });
